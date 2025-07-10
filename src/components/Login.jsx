bd8a9b5886210281b96b0414192570b597811bd9
apoorvbansal95 <apoorv95.ab@gmail.com>
2025-07-09 21:05:13 -0400

    redux-react

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constant';
 
 export default function Login() {
-
+    const navigate=useNavigate()
     const [emailId, setEmail] = useState("harshit@gmail.com")
     const [password, setPassword] = useState("Harshit@123")
-
+    const dispatch= useDispatch()
     function handleEmailChange(e) {
         setEmail(e.target.value)
 
@@ -16,16 +20,19 @@ export default function Login() {
 
     const handleLogin = async () => {
         try {
-            const res = await axios.post("http://localhost:3000/login", {
+            const res = await axios.post(BASE_URL+"/login", {
                 emailId,
                 password
             }, {withCredentials:true})
+            // console.log(res.data)
+            dispatch(addUser(res.data))
+            navigate("/")
         }
         catch (err) {
             console.log(err)
         }
     }
-    
+
     return (
         <div className='flex justify-center my-10'>
             <div className="card bg-base-300 w-96 shadow-sm">
