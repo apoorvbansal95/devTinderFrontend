fa414467a9803d7dd47f45e905ca32eeb31ef551
apoorvbansal95 <apoorv95.ab@gmail.com>
2025-07-19 19:39:44 -0400

    accept/reject

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constant'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
-import { addRequest } from '../utils/requestSlice'
+import { addRequest, removeRequest } from '../utils/requestSlice'
 
 export default function Requests() {
     const dispatch= useDispatch()
@@ -13,6 +13,7 @@ export default function Requests() {
             const allrequest= await axios.get(BASE_URL+"/user/requests/recieved", {withCredentials:true} )
             console.log(allrequest.data.data)
             dispatch(addRequest(allrequest.data.data))
+            
         }
         catch(err){
             console.log(err)
@@ -22,15 +23,27 @@ export default function Requests() {
         fetchRequest()
     },[])
 
+    if(!allRecievedrequests) return 
     if (allRecievedrequests.length===0){
-        return <p>No Pending Requests</p>
+        return <h1 className='flex justify-center my-10'>No Pending Requests</h1>
     }
+
+    const reviewRequest=async(status, req_id)=>{
+        try{
+            const res= await axios.post(BASE_URL+"/request/review/"+status+"/"+req_id, {}, {withCredentials:true})
+            dispatch(removeRequest(req_id))
+        }catch(err){
+            console.log(err)
+        }
+    }
+
 
   return (
     <div className='text-center my-5'>
       <h1 className='font-bold text-2xl'>Requests</h1>
         {allRecievedrequests.map((req)=>{
             const {firstName, lastName, photo, about}=req.fromUserId
+            const {_id}=req 
             return (
                 <div className='flex m-4 p-4 items-center bg-base-300 w-1/2 m-auto'>
                     <div>
@@ -42,10 +55,10 @@ export default function Requests() {
                     </div>
                     <div className='mx-9'>
                     <div className='flex my-2 '>
-                        <button className=' btn btn-soft btn-accent'>Accept</button>
+                        <button className=' btn btn-soft btn-accent'  onClick={()=>reviewRequest("accepted", _id)}>Accept</button>
                     </div>
                     <div className='flex btn btn-soft btn-error'>
-                        <button>Reject</button>
+                        <button onClick={()=>reviewRequest("rejected", _id)}>Reject</button>
                     </div>  
                     </div>                 
                 </div>
